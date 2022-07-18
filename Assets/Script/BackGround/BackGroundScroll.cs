26367c696c4e951f7852aa06015c848751dd1dc2
scv9804 <74706585+scv9804@users.noreply.github.com>
2022-07-19 01:26:42 +0900

    백그라운드 스크롤 제작중

--- Assets/Script/BackGround/BackGroundScroll.cs
+++ Assets/Script/BackGround/BackGroundScroll.cs
@@ -9,31 +9,45 @@ public class BackGroundScroll : MonoBehaviour
 	[SerializeField] GameObject Object_BackTree_2;
 	[SerializeField] GameObject Object_BackTree_3;
 
+	[SerializeField] GameObject Object_Ground_1;
+	[SerializeField] GameObject Object_Ground_2;
+
 	[SerializeField] GameObject EndPos;
 
+	bool is_moving;
 
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.K))
 		{
-			StartCoroutine(BackGround_StageMove());
+			is_moving = true;
 		}
+		if (is_moving)
+			Invoke("BackGround_StageMove", 0.01f);
 	}
 
 
-	public IEnumerator BackGround_StageMove() 
+	public void BackGround_StageMove() 
 	{
-		foreach (var Check in Objcet_Trees)
+		/*	foreach (var Check in Objcet_Trees)
+			{
+				Check.gameObject.transform.position += Vector3.left;
+			}*/
+
+
+		Object_Ground_1.transform.position += Vector3.left * Time.deltaTime * 10f;
+		Object_Ground_2.transform.position += Vector3.left * Time.deltaTime * 10f;
+
+
+		if (Object_Ground_1.gameObject.transform.position.x <= -26.0f)
 		{
-			Check.gameObject.transform.position += Vector3.left;
+			Object_Ground_1.gameObject.transform.position = new Vector3(26.0f, 0f ,1f);
 		}
-
-	/*	if ()
+		if (Object_Ground_2.gameObject.transform.position.x < -26.0f)
 		{
-
-		}*/
-
-		yield return true;	
+			Object_Ground_2.gameObject.transform.position = new Vector3(26.0f, 0f, 1f);
+			is_moving = false;
+		}
 	}
 
 
